ac993e3443109f088e9e69bdf9c9aa3610968206
Aaron Smith-Hayes <aaron@defydevcorp.com>
2022-02-19 19:32:50 -0700

    updated test

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,38 +3,33 @@ const { ethers } = require("hardhat");
 const { default: Web3 } = require("web3");
 
 describe("Mint testings", function () {
+  var openEvents;
+  
+  beforeEach(async function() {
+
+      const OpenEvents = await hre.ethers.getContractFactory("OpenEvents");
+      openEvents = await OpenEvents.deploy();
+  
+      await openEvents.deployed();
+  
+      console.log("OpenEvents deployed to:", openEvents.address);
+      await openEvents.createEvent(
+        "Jetgang",
+        1646323000,
+        true,
+        true,
+        hre.ethers.utils.parseEther("0.1"),
+        200,
+        false,
+        ""
+      );
+  
+  });
   it("Mint a single GA", async function () {
-    const [owner] = await ethers.getSigners();
-
-    const OpenEvents = await hre.ethers.getContractFactory("OpenEvents");
-    const openEvents = await OpenEvents.deploy();
-
-    await openEvents.deployed();
-
-    console.log("OpenEvents deployed to:", openEvents.address);
-    await openEvents.createEvent(
-      "Jetgang",
-      1646323000,
-      true,
-      true,
-      hre.ethers.utils.parseEther("0.1"),
-      200,
-      false,
-      ""
-    );
-
     await openEvents.buyTicket({ value: hre.ethers.utils.parseEther("0.1") });
     await openEvents.buyTicket({ value: hre.ethers.utils.parseEther("1") });
   });
   it("Mint VIP", async function () {
-    const [owner] = await ethers.getSigners();
-
-    const OpenEvents = await hre.ethers.getContractFactory("OpenEvents");
-    const openEvents = await OpenEvents.deploy();
-
-    await openEvents.deployed();
-
-    console.log("OpenEvents deployed to:", openEvents.address);
     await openEvents.addVIPPackage(
       0,
       hre.ethers.utils.parseEther("0.33"),
@@ -45,7 +40,7 @@ describe("Mint testings", function () {
     );
 
     await openEvents.buyVIPTicket({
-      value: hre.ethers.utils.parseEther("0.1"),
+      value: hre.ethers.utils.parseEther("0.33"),
     });
   });
 });
